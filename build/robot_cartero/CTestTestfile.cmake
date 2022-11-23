48d2d24bd21499c0c401ddaa5138e891c43a7ed9
Jose Angel Martin Gonzalez <jose.angel.martin2001@gmail.com>
2022-11-23 13:36:24 +0100

    Paquete del proyecto

---
+++ build/robot_cartero/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jamg/ROS/ProyectoRobotsMoviles/src/robot_cartero
+# Build directory: /home/jamg/ROS/ProyectoRobotsMoviles/build/robot_cartero
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
