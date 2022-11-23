768d34b5ed3d53c7ef2baebd1c831f831612ce1e
Jose Angel Martin Gonzalez <jose.angel.martin2001@gmail.com>
2022-11-23 14:19:54 +0100

    Pruebas reconocimiento de texto

---
+++ src/robot_cartero/scripts/text_recognition.py
@@ -0,0 +1,14 @@
+import cv2 as cv
+import pytesseract as pt
+
+image = cv.imread("imagenes/prueba.png")
+img_rgb = cv.cvtColor(image,cv.COLOR_BGR2RGB)
+
+print(pt.image_to_string(img_rgb))
+
+
+
+
+
+
+# Prueba de texto: 1, 2, 3
