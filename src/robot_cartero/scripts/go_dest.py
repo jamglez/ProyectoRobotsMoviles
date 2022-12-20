5f564573235822bbfc42dd6224e19b0f2074b36b
Daniel Frau Alfaro <98766327+DanielFrauAlfaro@users.noreply.github.com>
2022-12-20 19:34:00 +0100

    Nodo de ir a la posicion

--- src/robot_cartero/scripts/go_dest.py
+++ src/robot_cartero/scripts/go_dest.py
@@ -6,7 +6,9 @@ from std_msgs.msg import Bool
 import move_base_msgs.msg
 import actionlib
 
-rospy.init_node("go_node", PoseStamped)
+rospy.init_node("go_node")
+pub = rospy.Publisher("/arrive", Pose, queue_size=10)
+pub2 = rospy.Publisher("/go_pose", PoseStamped, queue_size=10)
 
 get_prev_pose = False
 prev_pose = Pose()
@@ -18,28 +20,27 @@ def cb(data):
             prev_pose = data.base_position.pose
 
 def go_pose(data):
-    global pub, prev_pose
+    global pub, prev_pose, get_prev_pose
     client = actionlib.SimpleActionClient('move_base', move_base_msgs.msg.MoveBaseAction)
-        
+    
     client.wait_for_server()
-        
+    
     desiredPose = PoseStamped()
 
     desiredPose.header.frame_id = "map"
-    # desiredPose.header.seq = 12
-    # desiredPose.header.stamp = rospy.Time.now()
-            
-    # desiredPose.pose = data.
-            
-    goal = move_base_msgs.msg.MoveBaseGoal(data.data)
+
+    goal = move_base_msgs.msg.MoveBaseGoal(data)
     print(goal)
+    
     client.send_goal(goal, feedback_cb=cb)
     client.wait_for_result()
-    
     pub.publish(prev_pose)
+    prev_pose = Pose()
+    get_prev_pose = False
     
     
 rospy.Subscriber("/go_pose", PoseStamped, go_pose)
-pub = rospy.Publisher("/arrive", Pose, queue_size=10)
 
-rospy.spin()
+if __name__ == "__main__":
+    while True:
+        pass
