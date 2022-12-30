ca798667e01e01bccc06fba520e0a9ac8afd2dc5
Jose Angel Martin Gonzalez <98955704+joseamg24@users.noreply.github.com>
2022-12-30 16:05:34 +0100

    Update
    text recognition y arm controller

--- src/robot_cartero/scripts/arm_controller.py
+++ src/robot_cartero/scripts/arm_controller.py
@@ -3,7 +3,7 @@
 import rospy
 from std_msgs.msg import Float64
 from std_msgs.msg import String
-import time
+
 
 class arm_controller():
 
@@ -13,7 +13,7 @@ class arm_controller():
         rospy.init_node("arm_controller")
 
         # Suscripcion a topics para coger y dejar carta
-        rospy.Subscriber("/arm_controller",String,self.arm_callback)
+        rospy.Subscriber("/arm_controller",String,self.__arm_callback)
 
         # Publica en topics del control del brazo y pinza
         self._shoulder = rospy.Publisher("/arm_shoulder_pitch_joint/command",Float64,queue_size=10)
@@ -21,13 +21,13 @@ class arm_controller():
         self._wrist = rospy.Publisher("/arm_wrist_pitch_joint/command",Float64,queue_size=10)
         self._gripper = rospy.Publisher("/arm_gripper_prismatic_joint/command",Float64,queue_size=10)
         
-    def arm_callback(self,data):
+    def __arm_callback(self,data):
         if data.data == "recoger":
             self.gripper_open()
             self.extend_arm()
-            time.sleep(6)
+            rospy.sleep(3)
             self.gripper_close()
-            time.sleep(3)
+            rospy.sleep(1.5)
             self.retract_arm()
             
         elif data.data == "soltar":
@@ -44,8 +44,8 @@ class arm_controller():
 
     def retract_arm(self):
         self._shoulder.publish(-1.5)
-        self._elbow.publish(-0.8)
-        self._wrist.publish(-0.0)
+        self._elbow.publish(-0) #Antes -0.8
+        self._wrist.publish(0)
 
     def gripper_open(self):
         self._gripper.publish(0)
@@ -63,4 +63,3 @@ if __name__ == "__main__":
     while not rospy.is_shutdown():
         
         rate.sleep()
-
--- src/robot_cartero/scripts/text_recognition.py
+++ src/robot_cartero/scripts/text_recognition.py
@@ -1,7 +1,127 @@
+#!/usr/bin/env python3
+
+import rospy
 import cv2 as cv
 import pytesseract as pt
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+from imutils.video import FPS
+from kobuki_msgs.msg import Sound
+# import keras_ocr
 
-image = cv.imread("imagenes/prueba.png")
-img_rgb = cv.cvtColor(image,cv.COLOR_BGR2RGB)
+class text_recognizer():
+    
+    def __init__(self) -> None:
 
-print(pt.image_to_string(img_rgb))
+        #Configuracion del reconocimiento texto de pytesseract
+        self.__confg = r'--oem 3 --psm 7 outputbase digits'
+        self.__count = 0
+        self.__number = 0
+        
+        # Nodo ROS
+        rospy.init_node("text_recognition")
+
+        # Suscriber a los topics: 
+        rospy.Subscriber('/camera/rgb/image_raw/compressed',Image,self.__camera_callback)
+        self.__bridge = CvBridge()
+
+        # Publisher en los topics:
+        self.__sound = rospy.Publisher("/mobile_base/commands/sound", Sound, queue_size=10) #LED tambien?
+
+
+    # Comprueba el numero de mesa durante varios frames
+    def __check_number(self,text):
+        for n in range(1,6):
+
+            if str(n) in text:
+                # print(n)
+                if self.__number == n:
+                        self.__count +=1
+                else:
+                    self.__number = n
+                    self.__count = 1
+
+
+    def webcam_test(self):
+        
+        fps = FPS().start()
+        cap = cv.VideoCapture(0)
+        cv.namedWindow('frame',cv.WINDOW_NORMAL)
+        cv.resizeWindow('frame',1280,720)
+        
+        while(True):
+
+            ret, frame = cap.read()
+        
+            if ret :
+
+                data = pt.image_to_data(frame,config=self.__confg)
+                text = pt.image_to_string(frame,config=self.__confg)
+
+                frame = self.bounding_box_text(data,frame)
+
+                self.__check_number(text)
+
+                if self.__count == 3:
+                    print(f'Mesa: {self.__number}')
+                    self.__count = 0
+
+                cv.imshow('frame', frame)
+            
+            fps.update()
+
+            if cv.waitKey(1) & 0xFF == ord('q'):
+                break
+        
+        fps.stop()
+        print(f"[INFO] elapsed time {round(fps.elapsed(), 2)}")
+        print(f"[INFO] approx. FPS : {round(fps.fps(), 2)}")
+         
+        cap.release()
+        cv.destroyAllWindows()
+        
+    def __camera_callback(self,image):
+        #Hacer que deje de detectar cuando no haga falta
+
+        cv_image = self.__bridge.imgmsg_to_cv2(image, desired_encoding='passthrough')
+
+        # data = pt.image_to_data(frame)
+        # frame = self.bounding_box_text(data,frame)
+
+        text = pt.image_to_string(cv_image,config=self.__confg)
+
+        self.__check_number(text)
+
+        if self.__count == 3:
+            print(f'Mesa: {self.__number}')
+            self.__sound.publish(1)
+            self.__count = 0
+
+        cv.imshow('frame', cv_image)
+        # cv.waitKey(1)
+            
+
+    #Dibujar las bounding boxes
+    def bounding_box_text(self,data,frame):
+                        
+        for z, a in enumerate(data.splitlines()):
+            if z!= 0:
+                a = a.split()
+                if len(a) == 12:
+                    x,y,width,height  = int(a[6]), int(a[7]),int(a[8]), int (a[9])
+                    cv.rectangle(frame, (x,y), (x+width,y+height), (255,0,0), 1)
+                    cv.putText(frame, a[11], (x,y+25), cv.FONT_HERSHEY_PLAIN,1,(0,0,255),2)
+
+        return frame
+
+
+if __name__ == "__main__":
+
+    recognizer = text_recognizer()
+
+    rate = rospy.Rate(10)
+
+    recognizer.webcam_test()
+
+    while not rospy.is_shutdown():
+        rate.sleep()
