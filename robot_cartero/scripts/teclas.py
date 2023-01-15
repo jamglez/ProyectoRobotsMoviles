09a92d938bff6cc45e013302f173534ac35e6eb9
Daniel Frau Alfaro <danielfraualfa@gmail.com>
2023-01-15 17:22:09 +0100

    Añadir todos los estados

--- robot_cartero/scripts/teclas.py
+++ robot_cartero/scripts/teclas.py
@@ -30,6 +30,63 @@ def callback(tecla):
     elif(str(tecla) == "'c'"):
         s.data = "c"
 
+    elif(str(tecla) == "'d'"):
+        s.data = "d"
+    
+    elif(str(tecla) == "'e'"):
+        s.data = "e"
+
+    elif(str(tecla) == "'f'"):
+        s.data = "f"
+
+    elif(str(tecla) == "'g'"):
+        s.data = "g"
+
+    elif(str(tecla) == "'h'"):
+        s.data = "h"
+
+    elif(str(tecla) == "'i'"):
+        s.data = "i"
+
+    elif(str(tecla) == "'j'"):
+        s.data = "j"
+
+    elif(str(tecla) == "'k'"):
+        s.data = "k"
+
+    elif(str(tecla) == "'l'"):
+        s.data = "l"
+
+    elif(str(tecla) == "'m'"):
+        s.data = "m"
+
+    elif(str(tecla) == "'n'"):
+        s.data = "n"
+
+    elif(str(tecla) == "'o'"):
+        s.data = "o"
+
+    elif(str(tecla) == "'p'"):
+        s.data = "p"
+
+    elif(str(tecla) == "'q'"):
+        s.data = "q"
+
+    elif(str(tecla) == "'r'"):
+        s.data = "r"
+
+    elif(str(tecla) == "'s'"):
+        s.data = "s"
+
+    elif(str(tecla) == "'t'"):
+        s.data = "t"
+
+    elif(str(tecla) == "'u'"):
+        s.data = "u"
+
+    elif(str(tecla) == "'v'"):
+        s.data = "v"
+
     pub.publish(s)
 
 
